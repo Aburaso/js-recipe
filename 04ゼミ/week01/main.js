64b92d3ca5bff8f101f63f9a8c45f80bd3dc2f46
Abraso <kachi_pea.61@docomo.ne.jpl>
2021-07-03 18:57:07 +0900

    クイズ

---
+++ 04ゼミ/week01/main.js
@@ -0,0 +1,18 @@
+const B_button=document.getElementById("basket-button");
+        const BB_button=document.getElementById("baseball-button");
+        const D_button=document.getElementById("dog-button");
+        const C_button=document.getElementById("cat-button");
+
+        B_button.onclick=function(){
+            alert("正解");
+        }
+        BB_button.onclick=function(){
+            alert("不正解");
+        }
+
+        D_button.onclick=function(){
+            alert("正解");
+        }
+        C_button.onclick=function(){
+            alert("不正解");
+        }
