41121060918c8a88c32db12c8c58ff59f2634dc7
Abraso <kachi_pea.61@docomo.ne.jpl>
2021-07-03 20:56:23 +0900

    fizz

--- 04ゼミ/week01/main.js
+++ 04ゼミ/week01/main.js
@@ -16,3 +16,18 @@ const B_button=document.getElementById("basket-button");
         C_button.onclick=function(){
             alert("不正解");
         }
+
+
+const Roy={
+    name:'ロイ',
+    age:20,
+    university:'立教',
+    hoby:["バスケ","洋楽"],
+    likeWord:"情熱",
+    likeComedian:"サンドウィッチマン",
+    likeColor:"緑",
+    syuukatsu:"嫌",
+    sayHello:function(){
+        alert("こんにちは");
+    },
+}
